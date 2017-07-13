09b5a48b15a2fd3db71713d9a0ba341e9f6bd36a
Michael Young (Liss) <Michael_Young1@ajg.com>
2017-07-13 23:20:08 +0100

    SVG animation on click
    added

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -11,7 +11,18 @@
   'use strict';
 
   $(function () {
-    // FastShell
+    document.getElementById("play").onclick = function () {
+      var rocket = document.getElementById("rocket");
+      var clouds = document.getElementById("clouds");
+      rocket.classList.add("rocket-animation");
+      clouds.classList.add("clouds-animation");
+
+      setTimeout(function() {
+        rocket.classList.remove("rocket-animation");
+        clouds.classList.remove("clouds-animation");
+      },3000);
+    };
   });
+
 
 })(jQuery, window, document);
